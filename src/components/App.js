a64a381d813de8f8df1c1af9c0728fa49d299c49
aboud <aboudiido@gmail.com>
2019-04-03 19:25:07 +0300

    'update'

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
     state = { videos: [], selectedVideo: null };
 
     componentDidMount() {
-        this.onTermSubmit('car');
+        
     }
 
     onTermSubmit = async term => {
